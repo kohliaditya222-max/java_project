802e1e128dc11fcada042980d00a58c942f8d950
kohliaditya222-max <kohliaditya222@gmail.com>
2026-09-22 11:09:01 +0530

    Update RoomAllotment.java

--- Main.java
+++ Main.java
@@ -1,42 +1,69 @@
-import java.util.Scanner;
+public class RoomAllotment{
+        static String[][] students;
+        static String[][] rooms;
 
-public class Main {
-
-    static void dataEntry(String[] students){
-        Scanner sc = new Scanner(System.in);
-
-        for(int i=0; i<students.length; i++){
-            System.out.println("Enter student "+(i+1)+" name:");
-            students[i] = sc.nextLine();
+        static void dataEntry(){
+            students = new String[][]{
+                {"Aditya", "Male"},
+                {"Priya", "Female"},
+                {"Rahul", "Male"},
+                {"Neha" , "Female"},
+                {"Aman", "Male"},
+                {"Rohit", "Male"},
+                {"Riya", "Female"},
+                {"kartikey","Male"},
+                {"karan","Male"},
+                {"sneha", "Female"},
+                {"Naina", "Female"},
+                {"Kavya", "Female"},
+                {"Sahil", "Male"},
+                {"Vikas", "Male"},
+                {"Anjali", "Female"},
+                {"Arjun", "Male"},
+                {"Divya", "Female"},
+                {"Manav", "Male"},
+                {"Aisha", "Female"},
+                {"Simran", "Female"},
+                {"Isha", "Female"},
+            };
         }
-    }
+        static void roomAllotment(){
+            rooms = new String[10][2];
+            boolean[] allocated = new boolean[students.length];
+            int room = 0;
 
-    static void roomAllotment(String[] students , String[][] rooms){
-        int studentIndex = 0;
+            for(int i=0; i<students.length && room < 10; i++){
+                if(allocated[i]){
+                    continue;
+                }
+                for(int j=i+1; j<students.length; j++){
+                    if(!allocated[j] && students[i][1].equals(students[j][1])){
+                        rooms[room][0] = students[i][0];
+                        rooms[room][1] = students[j][0];
 
-        for(int i=0; i<rooms.length;i++){
-            for(int j=0; j<rooms[i].length; j++){
-                if(studentIndex < students.length){
-                    rooms[i][j] = students[studentIndex];
-                    studentIndex++;
+                        allocated[i] = true;
+                        allocated[j] = true;
+
+                        room++;
+                        break;
+                    }
                 }
             }
-        }
-        if(studentIndex < students.length){
-            System.out.println("\nStudents without rooms : "+ students[studentIndex]);
+            System.out.println("\n-----ROOM ALLOCATION------");
+            for(int i=0; i<10; i++){
+                if(rooms[i][0] != null){
+                    System.out.println("Room" + (i+1) + " : "+ rooms[i][0] + " and " + rooms[i][1]);
+                }
+            }
+            System.out.println("\n----UNALLOCATED STUDENTS----");
+            for(int i=0; i<students.length; i++){
+                if(!allocated[i]){
+                System.out.println(students[i][0]);
+            }
         }
     }
-    public static void main(String[] args) {
-        String[] students = new String[21];
-        String[][] rooms = new String[10][2];
-
-        dataEntry(students);
-        roomAllotment(students, rooms);
-
-        System.out.println("\nRoom Allotment : ");
-
-        for(int i=0; i<rooms.length; i++){
-            System.out.println("Room "+ (i+1)+": "+rooms[i][0]+ " & "+ rooms[i][1]);
-        }
+    public static void main(String[] args){
+        dataEntry();
+        roomAllotment();
     }
 }
